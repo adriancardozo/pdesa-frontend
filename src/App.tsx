a5217a1054529f6db35dad8a6d44046895f10b54
Adrián Cardozo <cardozoadrian997@gmail.com>
2025-06-18 17:17:23 -0300

    Delete comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
 import './App.css';
-// import { Button, CssBaseline } from '@mui/material';
 import { CssBaseline } from '@mui/material';
 import Router from './component/router';
 
