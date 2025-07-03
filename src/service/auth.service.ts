28831373e1f82825411d1eee55d60aa75acc8c5b
Adrián Cardozo <cardozoadrian997@gmail.com>
2025-07-03 02:40:21 -0300

    Add username to nav bar

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { AxiosResponse } from 'axios';
 import { Service } from './service';
 import { AccessTokenResponse } from '../type/access-token-response.type';
+import { UserResponse } from '../type/user-response.type';
 
 export class AuthService extends Service {
   async login(username: string, password: string): Promise<AxiosResponse<AccessTokenResponse>> {
@@ -23,6 +24,10 @@ export class AuthService extends Service {
       username,
       password,
     });
+  }
+
+  async profile(): Promise<AxiosResponse<UserResponse>> {
+    return await this.httpClient.get('auth/profile');
   }
 }
 
